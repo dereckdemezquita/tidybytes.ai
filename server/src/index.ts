016f04566379546931307f6bd000f400ad88c616
dereckdemezquita <dereck@demezquita.com>
2023-06-29 08:37:52 -0500

    Update path.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-import authRouter from './auth';
+import authRouter from './routes/auth';
 import uploadRouter from './routes/uploadTab';
 
 dotenv.config();
