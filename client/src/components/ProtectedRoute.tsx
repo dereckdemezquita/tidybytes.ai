4746cbc4f7759b629593128bcfc34d15da9dcdca
dereckdemezquita <dereck@demezquita.com>
2023-07-02 12:19:10 -0500

    Document ProtectedRoute.tsx

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -11,12 +11,15 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     const location = useLocation();
     const token = localStorage.getItem('token');
 
+    // If there is no token (i.e., user is not authenticated), navigate to the landing page ('/')
     useEffect(() => {
         if (!token) {
             navigate("/");
         }
     }, [token, navigate]);
 
+    // If user is authenticated (i.e., there is a token), render the protected components (children)
+    // If not, redirect them to the landing page, keeping note of the current location they were trying to access
     return token ? children : <Navigate to="/" state={{ from: location }} replace />;
 };
 
