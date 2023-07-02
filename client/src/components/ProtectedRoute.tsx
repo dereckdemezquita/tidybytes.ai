7868af09e041df2578af205a91a5e23df2d183e0
dereckdemezquita <dereck@demezquita.com>
2023-07-02 12:18:56 -0500

    Document ProtectedRoute.tsx

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
-    children: React.ReactElement;  // Note that ReactNode includes 'undefined' which isn't a valid JSX element. Here, we use ReactElement instead.
+    children: React.ReactElement;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
@@ -19,4 +19,5 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
 
     return token ? children : <Navigate to="/" state={{ from: location }} replace />;
 };
+
 export default ProtectedRoute;
